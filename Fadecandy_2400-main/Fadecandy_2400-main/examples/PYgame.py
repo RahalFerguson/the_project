ea4a5b822c0f75f13484d22cef816a8cff9dfda6
RahalFerguson <rf517@live.mdx.ac.uk>
2022-02-28 17:38:37 +0000

    update

--- Fadecandy_2400-main/Fadecandy_2400-main/examples/PYgame.py
+++ Fadecandy_2400-main/Fadecandy_2400-main/examples/PYgame.py
@@ -1,22 +1,59 @@
 import pygame
-import pygame_menu
+from pygame import *
 
+# Color
+white = (255, 255, 255)
+black = (0, 0, 0)
+gray = (50, 50, 50)
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
+
+# Game Initialization
 pygame.init()
-surface = pygame.display.set_mode((600, 400))
 
-def set_difficulty(value, difficulty):
-    # Do the job here !
-    pass
+# Display Resolution
+width = 500
+height = 350
+Display = pygame.display.set_mode((width, height))
 
-def start_the_game(): # Do the job here !
-    pass
 
-menu = pygame_menu.Menu('Welcome' , 400, 300,
-theme=pygame_menu.themes. THEME_BLUE)
+def writeText(text, height_text, size, color):
+    font = pygame.font.SysFont("Arial", size, bold=True)  # Get font from pygame
+    text = font.render(text, 1, pygame.Color(color))  # draw text
+    text_rect = text.get_rect(center=(width // 2, height_text))  # this will be centered anyhow, but at y height
+    return text, text_rect
 
-menu.add.text_input('Name :',default= 'John Doe')
-menu.add. selector(' Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
-menu.add.button('Play', start_the_game)
-menu.add.button('Quit', pygame_menu.events.EXIT)
 
-menu.mainloop(surface)
+title_text, title_text_rect = writeText("Python Project", 20, 50, black)
+start_text, start_text_rect = writeText("START", 100, 20, blue)
+#exit_text, exit_text_rect = writetext("End", 100, 20, red)
+
+def menu():
+    while True:
+        Display.fill(white)
+        Display.blit(title_text, title_text_rect)
+
+        x, y = pygame.mouse.get_pos()
+
+        Start = Display.blit(start_text, start_text_rect)
+
+        if Start.collidepoint((x, y)):
+            if click:
+                # Call your functions here
+                print("Animation started")
+
+        click = False
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                return
+            elif event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True;
+
+        pygame.display.update()
+
+
+menu()
